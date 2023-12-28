c5affe9cf289fbe825c2d6f328e8bda479764a81
Vedika1008 <vedikasomani1008@gnail.com>
2023-12-28 22:37:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero.jsx"
+import Parallax from "./components/parallax/parallax.jsx";
+import Services from "./components/services/Services.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Cursor from "./components/cursor/Cursor.jsx";
+
+
+const App =()=>{
+ return(<div>
+  <Cursor/>
+   <section id="Homepage">
+    <Navbar/>
+   <Hero/>
+   </section>
+  <section id="Services">
+    <Parallax type='services'/>
+    </section>
+  <section>
+    <Services/>
+    </section>
+  <section id="Portfolio">
+    <Parallax type='portfolio'/>
+    </section>
+  <Portfolio/>
+  <section id="Contact">
+    <Contact/>
+  </section>
+  </div>)
+}
+
+export default App;
